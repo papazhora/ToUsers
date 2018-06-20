821f0b5f0641785373744120c1d884046154af37
Egor <4156464@gmail.com>
2018-06-20 14:03:38 +0300

    111

--- src/ru/company/Main.java
+++ src/ru/company/Main.java
@@ -1,4 +1,14 @@
 package ru.company;
+import ru.company.users.Users;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+            Users user = new Users();
+            System.out.print("Enter you name: ");
+            user.setName(scanner.nextLine());
+            System.out.print("You name is: " + user.getName());
+
+    }
 }
--- src/ru/company/users/Users.java
+++ src/ru/company/users/Users.java
@@ -1,4 +1,14 @@
 package ru.company.users;
 
 public class Users {
+    private String name;
+    private int age;
+    private String email;
+
+    public void setName(String userName) {
+        name = userName;
+    }
+    public String getName() {
+        return name;
+    }
 }
